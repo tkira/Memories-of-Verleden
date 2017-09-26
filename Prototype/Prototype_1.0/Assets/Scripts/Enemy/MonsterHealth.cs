4d1b082b83ac3a339f6859786320408f00c34a89
Tung <tung.dang@student.tafesa.edu.au>
2017-09-26 12:13:26 +0930

    Prototype 1.1

---
+++ Prototype/Prototype_1.0/Assets/Scripts/Enemy/MonsterHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHealth : MonoBehaviour {
+
+	public int monsterMaxHealth;
+	public int monsterHealth;
+
+	// Use this for initialization
+	void Start () {
+		monsterHealth = monsterMaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (monsterMaxHealth <= 0) {
+			gameObject.SetActive (false);
+
+		}
+	}
+
+	public void HurtMonster(int damageToGive){
+		monsterHealth -= damageToGive; 
+	}
+		
+
+}
---
+++ Prototype/Prototype_1.0/Assets/Scripts/Player/DamageEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemy : MonoBehaviour {
+
+	public int weaponDamage;
+	public GameObject damageEffect;
+	public Transform hitPoint;
+
+	public GameObject damageNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Enemy") {
+			//Destroy (other.gameObject);
+			other.gameObject.GetComponent<MonsterHealth> ().HurtMonster (weaponDamage);
+			Instantiate (damageEffect, hitPoint.position, hitPoint.rotation);
+			var clone = (GameObject)Instantiate (damageNumber, hitPoint.position, hitPoint.rotation);
+			clone.GetComponent<FloatingNumbers> ().damageNum = weaponDamage;
+		}
+	}
+}
